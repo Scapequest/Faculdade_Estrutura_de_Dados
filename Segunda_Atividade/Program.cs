345c3db63dbd4f1ee6eed741f171d95e76a77344
unknown <renanmastroto1@gmail.com>
2025-09-10 21:45:31 -0300

    Começado segunda atividade 'Pilha'

---
+++ Segunda_Atividade/Program.cs
@@ -0,0 +1,39 @@
+﻿int[] pilha = new int[20];
+int topo = 0;
+
+void Insere(int valor)
+{
+    pilha[topo] = valor;
+    topo = topo + 1;
+}
+
+int Remove()
+{
+    topo = topo - 1;
+    return (pilha[topo]);
+}
+
+bool EstaVazia()
+{
+    if (topo == 0)
+        return true;
+    else
+        return false;
+}
+
+bool EstaCheia()
+{
+    if (topo == 20)
+        return true;
+    else
+        return false;
+}
+
+int n = Convert.ToInt32(Console.ReadLine());
+while(n != 0)
+{
+    Insere(n%2);
+    n = n / 2; 
+}
+while(!EstaVazia())
+    Console.WriteLine(Remove());
