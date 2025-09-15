1901f6dd5210e09d75bccf5871e22af58aaf163e
Renan Mastroto <renanmastroto1@gmail.com>
2025-09-15 14:22:07 -0300

    Finalizado terceira atividade

---
+++ Terceira_Atividade/Program.cs
@@ -0,0 +1,50 @@
+﻿char[] pilha = new char[20];
+int topo = 0;
+
+void Insere(char valor)
+{
+  pilha[topo] = valor;
+  topo = topo + 1;
+}
+char Remove()
+{
+  topo = topo - 1;
+  return (pilha[topo]);
+}
+bool EstaVazia()
+{
+  if (topo == 0)
+      return true;
+  else
+      return false;
+}
+bool EstaCheia()
+{
+  if (topo == 20)
+    return true;
+  else
+    return false;
+}
+
+Console.WriteLine("Digite a frase: ");
+string entrada = Console.ReadLine();
+string saida = "";
+
+for (int i = 0; i <= entrada.Length; i++)
+{
+  if (i == (entrada.Length))
+  {
+    while (topo > 0)
+      saida = saida + Remove();
+  }
+  else if (entrada[i] != ' ')
+    Insere(entrada[i]);
+  else if (entrada[i] == ' ')
+  {
+    while (topo > 0)
+      saida = saida + Remove();
+    saida = saida + ' ';
+  }
+}
+
+Console.WriteLine(saida);
