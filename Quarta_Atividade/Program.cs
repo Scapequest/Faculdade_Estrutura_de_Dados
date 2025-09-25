bc3d0caa33c1c1962a30049004c9a647676da375
unknown <renanmastroto1@gmail.com>
2025-09-24 21:45:07 -0300

    Finalizado quarto exercício

--- Quarta_Atividade/Program.cs
+++ Quarta_Atividade/Program.cs
@@ -29,4 +29,31 @@ bool EstaCheio()
     return false;
 }
 
-Console.WriteLine("");
+Console.WriteLine("Quantos números quer digitar?");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+  Console.Write($"Digite o {i+1}º número: ");
+  Insere(Convert.ToInt32(Console.ReadLine()));
+}
+
+int maior = fila[inicio];
+int menor = fila[inicio];
+int soma = 0;
+
+while (!EstaVazia())
+{
+  int temp = Remove();
+  if (temp > maior)
+    maior = temp;
+  if (temp < menor)
+    menor = temp;
+  soma = soma + temp;
+}
+
+float media = (float)soma / n;
+
+Console.WriteLine($"Maior número: {maior}");
+Console.WriteLine($"Menor número: {menor}");
+Console.WriteLine($"Média Aritmética: {media}");
